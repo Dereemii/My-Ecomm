655945a078da8cc6322d92740993443bfd6f159a
Leslie Herrera <lherrera@ecomsur.com>
2021-10-18 22:01:25 -0300

    indentation

--- src/components/ItemCount.js/ItemCount.jsx
+++ src/components/ItemCount.js/ItemCount.jsx
@@ -7,27 +7,24 @@ import { useState } from 'react';
 const ItemCount = ({ initial, stock }) => {
 
     const [count, setCount] = useState(initial)
-  /*   const stockText = useRef() */
 
     const onDecrease = () => {
         if (count >= initial) {
-         setCount(count - 1)
-       }
-     }
- 
-     const onIncrease = () => {
-       if (count < stock) {
-         setCount(count + 1)
-       }
-     }
+            setCount(count - 1)
+        }
+    }
 
- 
+    const onIncrease = () => {
+        if (count < stock) {
+            setCount(count + 1)
+        }
+    }
 
     return (
         <>
             <div className="cardComponent">
                 <div className="imgCard">
-                    <img src="https://via.placeholder.com/150" alt="" />
+                    <img src="https://via.placeholder.com/150" alt="imagen de prueba" />
                 </div>
                 <h3>Product Name</h3>
                 <div className="counterCard">
@@ -40,7 +37,7 @@ const ItemCount = ({ initial, stock }) => {
                         <button onClick={onIncrease}><FontAwesomeIcon icon={faPlus} /></button>
                     </span>
                 </div>
-                <span style={{ display: count > 4 ? 'block' : 'none' }}  className="stockOver">Stock máximo alcanzado</span>
+                <span style={{ display: count > 4 ? 'block' : 'none' }} className="stockOver">Stock máximo alcanzado</span>
                 <div className="addToCart">Add to Cart <FontAwesomeIcon icon={faShoppingCart} /></div>
             </div>
         </>
