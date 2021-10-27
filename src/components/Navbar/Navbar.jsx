6714e5e23955087254d5ce44fc5326c69548e8c5
Leslie Herrera <lherrera@ecomsur.com>
2021-10-27 00:32:24 -0300

    navbugs ok

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import CardWidget from './CardWidget';
+
+export const Navbar = () => {
+
+    return (
+        <nav>
+            <ul>
+                <div>
+                    <li>Logo</li>
+                    <li>About Us</li>
+                    <li>Out Products</li>
+                </div>
+
+                <div>
+                    <li><FontAwesomeIcon icon={faSearch} /></li>
+                    <CardWidget />
+                    <li><FontAwesomeIcon icon={faUser} /></li>
+                </div>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
