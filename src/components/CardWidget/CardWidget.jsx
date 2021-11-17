01049ef69a995f11b116250ddfaeaa95aedb68c2
Leslie Herrera <lherrera@ecomsur.com>
2021-11-16 22:38:43 -0300

    changes styles in cart

--- src/components/CardWidget/CardWidget.jsx
+++ src/components/CardWidget/CardWidget.jsx
@@ -1,16 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-
+import { Link } from "react-router-dom";
 import './CardWidget.css'
 
 const CardWidget = () => {
     return (
         <>
-            <div className="cartCount">
-                <li><FontAwesomeIcon icon={faShoppingCart} />
-                    <li>2</li></li>
-
-            </div>
+            <Link to="/cart">
+                <div className="cartCount">
+                    <li><FontAwesomeIcon icon={faShoppingCart} />
+                        <li>2</li>
+                    </li>
+                </div>
+            </Link>
         </>
     );
 }
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,11 @@
-import { CartContext } from '../../context/cartContext'
-import { useContext } from 'react';
+//import { CartContext } from '../../context/cartContext'
+//import { useContext } from 'react';
 //import ItemCount from '../ItemCount/ItemCount';
 //import { Link } from 'react-router-dom';
 
 const Cart = () => {
 
-    const { cart, removeItem, clearCart } = useContext(CartContext);
+    //const { cart, removeItem, clearCart } = useContext(CartContext);
     return (
         <><h1>Mi carrito de compras</h1></>
     );
